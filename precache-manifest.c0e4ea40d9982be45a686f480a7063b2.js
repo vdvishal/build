da06014b91cfeac1f439e7275126898a8f23b85b
Vishal Dutta <36704086+vdvishal@users.noreply.github.com>
2019-11-16 15:43:45 +0530

    Initial commit

---
+++ precache-manifest.c0e4ea40d9982be45a686f480a7063b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf443810386414721e0d81c73de9015d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8cbe064759b962bc8934",
+    "url": "/static/css/2.7ececfe9.chunk.css"
+  },
+  {
+    "revision": "8abb13e36bdaac996c9a",
+    "url": "/static/css/main.a07b840a.chunk.css"
+  },
+  {
+    "revision": "8cbe064759b962bc8934",
+    "url": "/static/js/2.8f5f0761.chunk.js"
+  },
+  {
+    "revision": "8abb13e36bdaac996c9a",
+    "url": "/static/js/main.9aef9462.chunk.js"
+  },
+  {
+    "revision": "30727a7201c48f962e90",
+    "url": "/static/js/runtime-main.fc33f77a.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
